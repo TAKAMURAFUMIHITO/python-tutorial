449fe26cd017cc9555a9d55a5acb526b2b996a81
TAKAMURAFUMIHITO <108682238+TAKAMURAFUMIHITO@users.noreply.github.com>
2023-07-04 18:50:15 +0900

    if文

---
+++ elif.py
@@ -0,0 +1,49 @@
+x = 0
+if x == 0:
+    print('x is zero')
+elif x == 1:
+    print('x is one')
+else:
+    print('x is other')
+
+print("-------------------")
+
+if 1:
+    print('True') # Trueが出力される
+else:
+    print('False')
+ 
+if 0:
+    print('True')
+else:
+    print('False') # Falseが出力される
+     
+if 0.0:
+    print('True')
+else:
+    print('False') # Falseが出力される
+     
+if 0.1:
+    print('True') # Trueが出力される
+else:
+    print('False')
+ 
+if []:
+    print('True')
+else:
+    print('False') # Falseが出力される
+ 
+if ():
+    print('True')
+else:
+    print('False') # Falseが出力される
+ 
+if None:
+    print('True')
+else:
+    print('False') # Falseが出力される
+ 
+if "":
+     print('True')
+else:
+    print('False') # Falseが出力される
